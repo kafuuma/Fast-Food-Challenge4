f9ed2056d28d65df8df74d1863a3f70f7648f8ca
kafuuma <arkafuuma@gmail.com>
2018-10-18 12:26:09 +0300

    chore(connect UI pages ): interlink UI pages with js
    This commit delivers UI pages interlinked properly wit Java Script
    
    [Delivers #12673538363]

---
+++ UI/js/signup.js
@@ -0,0 +1,49 @@
+
+var  details;
+window.addEventListener("load", start_signup);
+function start_signup(){
+    let signup = document.getElementById("signup");
+    signup.addEventListener("submit", signUp);
+}
+
+function signUp(e){
+    e.preventDefault();
+    console.log("method signup called");
+    var name = document.getElementById("full_name").value;
+    var phone = document.getElementById("phone").value;
+    var email_address= document.getElementById("email1").value;
+    var password_ = document.getElementById("password1").value;
+    var confirm_password_ = document.getElementById("password2").value;
+
+    user_details = {
+            full_name: name,
+            contact: phone,
+            email: email_address,
+            password: password_,
+            confirm_password: confirm_password_
+    }
+fetch(
+
+    
+    "http://127.0.0.1:5000/api/v1/auth/signup",
+    {
+        method: "POST",
+        headers:{
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(user_details)
+    })
+.then(function(res){
+    return res;
+})
+.then(function(resdata){
+  console.log(resdata);
+  console.log(resdata.json(["message"]));
+  console.log(resdata.status)
+})
+.catch(function(error){
+    console.log(error)
+});
+}
+
+
