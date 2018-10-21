2eeb72fb6041e0c31574ccb1fd45854604a99818
kafuuma <arkafuuma@gmail.com>
2018-10-21 17:52:20 +0300

    feat(fetch orders):user can fetch history of orders
    This commit delivers a feature to fecth all orders
    by a user
    
    [# 161321603]

--- UI/js/orders.js
+++ UI/js/orders.js
@@ -1,10 +1,73 @@
 
+
+//access token
+console.log(localStorage["auth-token"]);
+let url1="http://127.0.0.1:5000/api/v1/users/orders";
+let url2="http://127.0.0.1:5000/api/v1/orders";
+
+window.addEventListener("load", startOrderActions);
+function startOrderActions(){
+    let logout = document.getElementById("log-out");
+    let placeorder = document.getElementById("cat_log");
+    let vieworder = document.getElementById("history");
+    let allorders = document.getElementById("allorders");
+    let update_status = document.getElementById("status");
+    logout.addEventListener("click", logOut, false);
+    if(placeorder){
+        placeorder.addEventListener("submit", place_order, false);
+    }
+    if (vieworder){
+        console.log(vieworder)
+        vieworder.addEventListener("click", Fetch_orders,false);
+    }
+
+}
+
 //customise it to ordes
-function fetch_all_menu(e){
+function place_order(e){
     e.preventDefault();
-    console.log("fetch all menu called");
-    
-    fetch(url,
+    let selected = document.querySelectorAll(".checkbox");
+    console.log("place_order method called");
+    let orders =[];  
+    selected.forEach(function(menu_item){
+        if (menu_item.checked){
+            orders.push(parseInt(menu_item.id));
+            console.log(orders);
+        }
+    }) 
+     order_details={"menu_id":orders[0]};
+   
+    fetch(url1,
+        {
+            method: "POST",
+            headers:{
+                "content-type":"application/json",
+                "Authentication":localStorage["auth-token"]
+            },
+            body:JSON.stringify(order_details)
+        })
+        .then(function(res){
+            return res.json()
+        })
+        .then(function(resdata){
+            console.log(resdata["message"]);
+            if(resdata["message"]=="successfuly placed food order"){
+            document.getElementById("cat-box").style.display="none";
+            alert(resdata["message"]);
+        }
+        else{
+            console.log(resdata["message"]);
+        }
+        })  
+        .catch(function(error){
+            console.log(error)
+        });
+    }
+
+function Fetch_orders(e){
+    e.preventDefault();
+    console.log("fetch_order method called");
+    fetch(url1,
         {
             method: "GET",
             headers:{
@@ -18,44 +81,46 @@ function fetch_all_menu(e){
         })
         .then(function(resdata){
             console.log(resdata["message"]);
-            console.log(resdata["menu"]);
-            if(resdata["message"]=="successfuly fetched all menu"){
-            let menu =resdata["menu"];
-            let header = `
+            if(resdata["message"]=="successfully fetched orders"){
+                console.log(resdata["orders"]);
+                let orders =resdata["orders"];
+                let header = `
     
             <table class="table-data" id="admin-history">
                 <thead>
                     <tr>
-                        <th>Menu_Id</th>
-                        <th>Order</th>
-                        <th>Description</th>
-                        <th>Menu Price</th>
-                        <th>Menu img</th>
+                    <td>Order ID</td>
+                    <td>Menu ID</td>
+                    <td>Order Satatus</td>
+                    <td>Date</td>
+                    <td>Delete</td>
                     </tr>
                 </thead>
                 <tbody>
             
             `;
-            menu.forEach(function(menu_item){
-              header  += `
-                <tr>
-                    <td>${menu_item.menu_id}</td>
-                    <td>${menu_item.menu_name}</td>
-                    <td>${menu_item.description}</td>
-                    <td>${menu_item.menu_price}</td>
-                    <td>${menu_item.menu_image}</td>
+            orders.forEach(function(order){
+                header  += `
+                </tr>
+                    <td>${order.order_id}</td>
+                    <td>${order.menu_id}</td>
+                    <td>${order.status}</td>
+                    <td>9th.Oct.2018</td>
+                    <td><button>Trash</button></td>
                 </tr>
                 `;
             });
             let display_menu = header+"</tbody></table>";
-              
-            //    window.location.reload();
-               document.getElementById("fetch_menu").innerHTML = display_menu;
-               document.getElementById("dropdown").style.display="none";
+                
+                document.getElementById("dropdown").style.display="none";
+                document.getElementById("cat-box").style.display="none";
+                document.getElementById("user_orders").innerHTML = display_menu;
             }
             else{
-                document.location.reload();
-                document.getElementById("order-block").innerHTML = resdata["message"];
+                document.getElementById("dropdown").style.display="none";
+                document.getElementById("cat-box").style.display="none";
+                document.getElementById("user_orders").innerHTML = display_menu;
+                document.getElementById("user_orders").innerHTML = resdata["message"];
             }
             alert(resdata["message"]);
         })  
@@ -63,8 +128,6 @@ function fetch_all_menu(e){
             console.log(error)
         });
         }
-
-
 
 function logOut(){
     localStorage.setItem("auth-token",null);
