380bbf87848e1a30fa9bda642bac45bed140c08b
kafuuma <arkafuuma@gmail.com>
2018-10-25 23:21:46 +0300

    order status

--- UI/js/orders.js
+++ UI/js/orders.js
@@ -290,7 +290,7 @@ function Update_order_status(){
             parent = parent.parentNode
     }
     var order_id, child;
-    var data = {}
+    const data = {}
     for ( var i = 0, _len = parent.children.length; i < _len; i++ ) {
         console.log(i)
         child = parent.children[i];
